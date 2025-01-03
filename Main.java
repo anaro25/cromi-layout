939cccb6b61bbfbb78bb1f85e1d4e3e85c6f8ad2
anaro25 <daryllzaballa01@gmail.com>
2025-01-03 17:53:00 +0800

    implemented arraylist instead of static array

--- Main.java
+++ Main.java
@@ -1,52 +1,49 @@
 package x;
 
-// This programs aims to do Huffman coding to waste no time.
-	// Just emulate the manual encoding in excel (represent as cells)
-	// You can just use static arrays for clearer intuition.
+import java.util.ArrayList;
 
 public class Main {
 	
 	public static void main (String[] args) {
 		int num_keys = 90; // number of keys
-		int maxCodeLength = 5; // based
+		int maxCodeLength = 5;
 		
-		String[][] sheet = new String[num_keys * 2][1 + 2*(maxCodeLength)];
-			// double the row size to allow space for block migrationsc
-			// represent the keys using indices (whole numbers)
-			// leave a column for internal_node_names to the left of frequency column
-		sheet = formatEmptyCells(sheet.length, sheet[0].length);
+		ArrayList<ArrayList<String>> stringSheet = new ArrayList<>();
+		ArrayList<ArrayList<Double>> doubleSheet = new ArrayList<>();
+		
+		// initialize all cells to be null. This is important since you tend to leave many cells empty.
+		for (int row = 0; row < num_keys; row++) {
+			stringSheet.add(new ArrayList<>());
+            doubleSheet.add(new ArrayList<>());
+            
+			for (int col = 0; col < maxCodeLength; col++) {
+				stringSheet.get(row).add(null);
+				doubleSheet.get(row).add(null);
+			}
+		}
 		
 		// assign x-values
-		for (int row = 0; row <= num_keys; row++) {
-			double x = (double) row;
-			double y = p(x);
-			sheet[row][0] = "  " + String.format("%02d", (int) x) + " ";
-			sheet[row][4] = Double.toString(y);
+		for (int x = 1; x <= num_keys; x++) {
+			double y = p((double) x);
+			
+			stringSheet.get(x - 1).set(0, Formatting.formatFunctionLabel(x)); // "001", etc.
+			doubleSheet.get(x - 1).set(1, y);
 		}
 		
 		// test print
-		for (int row = 0; row < sheet.length; row++) {
-			System.out.print("| " + sheet[row][0] + " || ");
-			
-			// start at 1 since sheet[row][0] is already printed
-			for (int col = 1; col < sheet[0].length; col++) {
-				System.out.print(formatValue(sheet[row][col]) + " | ");
+		for (int row = 0; row < stringSheet.size(); row++) {
+			for (int col = 0; col < stringSheet.get(0).size(); col++) {
+				if (col == 0) {
+					System.out.print(" | " + stringSheet.get(row).get(col));
+				}
+				else {
+					System.out.print(" | " + doubleSheet.get(row).get(col));
+				}
 			}
 			System.out.println();
 		}
 	}
-	
-	private static String[][] formatEmptyCells(int rowSize, int colSize) {
-		String[][] emptySheet = new String[rowSize][colSize];
-		
-	    for (int row = 0; row < rowSize; row++) {
-	        for (int col = 0; col < colSize; col++) {
-	            emptySheet[row][col] = "  +  "; // Set each cell to "  +  "
-	        }
-	    }
-		return emptySheet;
-	}
-	
+	/*
 	private static String formatValue(String value) {
 	    int length = 5;
 	    
@@ -67,8 +64,21 @@ public class Main {
 	    }
 	    return paddedValue.toString();
 	}
+	*/
 	
 	private static double p(double x) { // Desmos exp regression
 		return 10.04681 * Math.pow(0.906906, x);
+	}
+}
+
+class Formatting {
+	public static String formatFunctionLabel(int x) { // 001, 002, etc.
+		String label = Integer.toString(x);
+		int labelLength = 3;
+		
+		for (int i = 0; i < labelLength - label.length(); i++) {
+			label = "0" + label;
+		}
+		return label;
 	}
 }
