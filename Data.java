0ec8d5c8facb1523be2d43170e8dec84bba31ca9
anaro25 <daryllzaballa01@gmail.com>
2025-01-05 17:10:46 +0800

    added files

---
+++ Data.java
@@ -0,0 +1,8 @@
+package x;
+import java.util.ArrayList;
+
+public class Data {
+	final static int degree = 9; // degree-ary
+	
+	static ArrayList<ArrayList<Node>> levelOrderSiblings = new ArrayList<>();
+}
---
+++ Test.java
@@ -0,0 +1,33 @@
+package x;
+import java.util.ArrayList;
+
+public class Test {
+		
+	public static void printLevelOrder() {
+		for (ArrayList<Node> siblingGroup : Data.levelOrderSiblings) {
+            for (Node node : siblingGroup) {
+            	System.out.print("(" + node.symbolName + ") ");
+                System.out.print(Printer.formatDouble(node.freq) + "\n");
+            }
+            System.out.println();
+        }
+	}
+	
+	public static void printSumAllNodes() {
+		double sumAllNodes = 0;
+		for (ArrayList<Node> siblings : Data.levelOrderSiblings) {
+			for (Node node : siblings) {
+				sumAllNodes += node.freq;
+			}
+		}
+		System.out.println("sumAllNodes: " + sumAllNodes);
+	}
+	
+	public static void printSumSymbols() {
+		double sumSymbols = 0;
+		for (Symbol symbol : SymbolAssigner.symbolList) {
+			sumSymbols += symbol.freq;
+		}
+		System.out.println("sumSymbols: " + sumSymbols);
+	}
+}
